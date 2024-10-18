2e72d8830779eafc44b41f30ec91da7648331f75
phie <74067483+phie-war-hier@users.noreply.github.com>
2024-10-18 12:54:41 +0200

    Dynamic Posts Basics

--- main.js
+++ main.js
@@ -21,20 +21,23 @@ function navigateToExperience() {
 const portfolioEntries = [
     {
         projectCoverimage: "img/hdil-smartroots-coverbild-400.webp",
-        projectTag: "User Experience",
+        alt: "User Experience",
+        projectTag: ["User Experience", "Interaction"],
         projectYear: 2023,
+        projectDescription: "sMArt roots is a project by Smart City Mannheim and HDIL. The aim was to create interactive visualizations with the city's citizens as the focus group."
+    },
+    {
+        projectCoverimage: "img/hdil-ar-data-experience-point-coverbild-400.webp",
+        alt: "User Experience",
+        projectTag: ["Data Vis", "Interaction"],
+        projectYear: 2024,
         projectDescription: "A description of the project"
     },
     {
-        projectCoverimage: "https://example.com/webdesign.jpg",
-        projectTag: "Interaction",
-        projectYear: 2022,
-        projectDescription: "A description of the project"
-    },
-    {
-        projectCoverimage: "https://example.com/webdesign.jpg",
-        projectTag: "Interaction",
-        projectYear: 2022,
+        projectCoverimage: "img/sh-irrlicht-coverbild-400.webp",
+        alt: "User Experience",
+        projectTag: ["Interaction", "Data Vis", "Installation"],
+        projectYear: 2024,
         projectDescription: "A description of the project"
     }
 ];
@@ -47,10 +50,19 @@ portfolioEntries.forEach(entry => {
     const entryDiv = document.createElement('div');
     entryDiv.classList.add('projects-element');
 
+
+    let tagsHTML = '<ul class="project-tags">';
+    entry.projectTag.forEach(tag => {
+        tagsHTML += `<li class="tag">${tag}</li>`;
+    });
+    tagsHTML += '</ul>';
+
+
+
     // HTML-Inhalt für den Eintrag erstellen
     entryDiv.innerHTML = `
-        <img class="project-coverimage" src="${entry.projectCoverimage}" alt="${entry.projectTag}">
-        <div class="project-tag">${entry.projectTag}</div>
+        <img class="project-coverimage" src="${entry.projectCoverimage}" alt="${entry.alt}">
+        ${tagsHTML}
         <p class="project-year">(${entry.projectYear})</p>
         <p class="project-description">${entry.projectDescription}</p>
     `;
@@ -58,4 +70,5 @@ portfolioEntries.forEach(entry => {
     // Den Eintrag in das Portfolio-Div einfügen
     portfolioContainer.appendChild(entryDiv);
 });
+
 

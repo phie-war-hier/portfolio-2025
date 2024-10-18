7ac4456474e0b85766cf06484665ec4f83a464f8
phie <74067483+phie-war-hier@users.noreply.github.com>
2024-10-18 14:49:39 +0200

    Navigation Logic + Experience

--- main.js
+++ main.js
@@ -5,20 +5,31 @@ function changeLang() {
 
 
 function navigateToShow() {
-    console.log("Show");
+    document.getElementById('show').style.display = "block";
+    document.getElementById('projects').style.display = "none";
+    document.getElementById('experience').style.display = "none"; 
 }
 
 function navigateToProjects() {
-    console.log("Project");
+    document.getElementById('show').style.display = "none";
+    document.getElementById('projects').style.display = "flex";
+    document.getElementById('experience').style.display = "none";
 }
 
 function navigateToExperience() {
-    console.log("Experience");
+    document.getElementById('show').style.display = "none";
+    document.getElementById('projects').style.display = "none";
+    document.getElementById('experience').style.display = "flex";
 }
 
 
 
-const portfolioEntries = [
+
+
+
+
+
+const projectsEntries = [
     {
         projectCoverimage: "img/hdil-smartroots-coverbild-400.webp",
         alt: "User Experience",
@@ -31,21 +42,21 @@ const portfolioEntries = [
         alt: "User Experience",
         projectTag: ["Data Vis", "Interaction"],
         projectYear: 2024,
-        projectDescription: "A description of the project"
+        projectDescription: "The AR DXP at HDIL is a research work on embedded temporal data visualizations in an urban environment."
     },
     {
         projectCoverimage: "img/sh-irrlicht-coverbild-400.webp",
         alt: "User Experience",
         projectTag: ["Interaction", "Data Vis", "Installation"],
         projectYear: 2024,
-        projectDescription: "A description of the project"
+        projectDescription: "Irrlicht - A data-supported, interactive and immersive installation to raise awareness of the issue of light pollution."
     }
 ];
 
 
-const portfolioContainer = document.querySelector('.projects-container');
+const projectsContainer = document.querySelector('.projects-container');
 
-portfolioEntries.forEach(entry => {
+projectsEntries.forEach(entry => {
     // Ein neues Div für jeden Eintrag erstellen
     const entryDiv = document.createElement('div');
     entryDiv.classList.add('projects-element');
@@ -64,11 +75,55 @@ portfolioEntries.forEach(entry => {
         <img class="project-coverimage" src="${entry.projectCoverimage}" alt="${entry.alt}">
         ${tagsHTML}
         <p class="project-year">(${entry.projectYear})</p>
-        <p class="project-description">${entry.projectDescription}</p>
+        <p class="description">${entry.projectDescription}</p>
     `;
 
     // Den Eintrag in das Portfolio-Div einfügen
-    portfolioContainer.appendChild(entryDiv);
+    projectsContainer.appendChild(entryDiv);
 });
 
 
+
+
+
+
+const experienceEntries = [
+    {
+        experienceTag: "Degree",
+        experienceYear: 2024,
+        experienceDescription: "Bachelor Degree in Communication Design at the Faculty of Design Mannheim"
+    },
+    {
+        experienceTag: "Publication",
+        experienceYear: 2024,
+        experienceDescription: "AR DXP Publication with HDIL."
+    },
+    {
+        experienceTag: "Jobposition",
+        experienceYear: 2024,
+        experienceDescription: "Graphic designer at HDIL."
+    }
+];
+
+
+const experienceContainer = document.querySelector('.experience-container');
+
+experienceEntries.forEach(entry => {
+    // Ein neues Div für jeden Eintrag erstellen
+    const entryDiv = document.createElement('div');
+    entryDiv.classList.add('experience-element');
+
+
+
+    // HTML-Inhalt für den Eintrag erstellen
+    entryDiv.innerHTML = `
+        <div class="experience-tags">  
+            <div class="tag">${entry.experienceTag}</div>
+            <p class="experience-year">${entry.experienceYear}</p>
+        </div>
+        <p class="description">${entry.experienceDescription}</p>
+    `;
+
+    // Den Eintrag in das Portfolio-Div einfügen
+    experienceContainer.appendChild(entryDiv);
+});
